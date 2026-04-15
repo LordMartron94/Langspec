8c19418a6501cb2212120e3fd379fcc1963fc4fa
Matthew van der Hoorn <md.career@protonmail.com>
2026-04-15 20:05:48 +0200

    add types for editor stuff to bootstrap

--- bootstrap/types.go
+++ bootstrap/types.go
@@ -1,5 +1,12 @@
 package bootstrap
 
-import "langspec"
+import (
+	"langspec"
+	"langspec/editor"
+)
 
 type LangParser[TNodeKind ~uint32] = langspec.LangParser[rune, string, uint32, uint32, TNodeKind]
+
+type EditorCtx = editor.EditorCtx[rune, uint32, uint32, string, uint32]
+
+type EditorOverride[TEditorCtx any] = editor.EditorOverride[rune, uint32, uint32, string, uint32, TEditorCtx]
